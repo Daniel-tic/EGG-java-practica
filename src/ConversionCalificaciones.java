207d81ddfcb014ee3710dea88329363ed0d55ef3
Daniel-tic <daniel.tic2300@gmail.com>
2024-07-02 20:21:41 -0500

    conversion de calificaciones

---
+++ src/ConversionCalificaciones.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ConversionCalificaciones {
+
+    public static void main(String[] args) {
+        System.out.println("ingresa una calificacion del 1 al 5 : ");
+        Scanner sc = new Scanner(System.in);
+        int cal = sc.nextInt();
+        switch (cal) {
+            case 1:
+                System.out.println("muy deficiente");
+                break;
+            case 2:
+                System.out.println("deficiente");
+                break;
+            case 3:
+                System.out.println("suficiente");
+                break;
+            case 4:
+                System.out.println("notable");
+                break;
+            case 5:
+                System.out.println("sobresaliente");
+                break;
+            default:
+                System.out.println("la calificacion no es valida");
+        }
+    }
+}
